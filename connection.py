fc09ab49708479c27d0246a4c2f8af6515d2284e
Mike Barcelos <mikebbatista@hotmail.com>
2021-01-07 20:50:00 -0300

    inicio da conexão com o banco

---
+++ connection.py
@@ -0,0 +1,16 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+
+conn.close()
